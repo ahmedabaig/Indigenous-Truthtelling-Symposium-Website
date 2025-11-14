ef54d75bf2d46923e8ad0a2f97d3b745a24738a5
Johnny Wu <johwuy@gmail.com>
2025-11-14 19:31:22 +0000

    Add footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Divider from "./divider";
+
+function Footer() {
+  return (
+    <footer>
+      <Image
+        src="/image2.webp"
+        alt="Blossoming Under Seige by Mona Kadah"
+        width={1544}
+        height={1166}
+        className="w-full h-auto opacity-34"
+      />
+      <Divider color1="#F37E6A" color2="#F1BE4B"></Divider>
+    </footer>
+  );
+}
+
+export default Footer;
