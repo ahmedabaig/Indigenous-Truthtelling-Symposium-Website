779d9415af247f5c1ab99483aa52d76e2aa9b951
Johnny Wu <johwuy@gmail.com>
2025-11-15 23:49:16 +0000

    Make footer not draggable

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -10,6 +10,7 @@ function Footer() {
         width={1544}
         height={1166}
         className="w-full h-auto opacity-34"
+        draggable="false" 
       />
       <Divider color1="#F37E6A" color2="#F1BE4B"></Divider>
     </footer>
