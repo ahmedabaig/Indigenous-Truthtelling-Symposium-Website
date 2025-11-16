8bc6dc07786f5c7b11a024f8ebc3c2dd2e356b6d
Johnny Wu <johwuy@gmail.com>
2025-11-16 00:08:02 +0000

    CReate page

--- app/(coming-soon)/coming-soon/page.tsx
+++ app/(coming-soon)/coming-soon/page.tsx
@@ -1,5 +1,9 @@
 function ComingSoon() {
-  return <h1>Landing</h1>;
+  return (
+    <div className='h-screen w-screen bg-cover bg-center bg-no-repeat bg-[linear-gradient(rgba(0,0,0,0.65),rgba(0,0,0,0.65)),url("/image2.webp")] flex justify-center items-center'>
+      <p className="text-creme text-7xl center italic">Coming soon December 2, 2025</p>
+    </div>
+  );
 }
 
 export default ComingSoon;
