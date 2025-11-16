02415042f36462a60728f41a843d9a9d3add4dcd
Johnny Wu <johwuy@gmail.com>
2025-11-16 19:33:57 +0000

    Add clicking feature to button.

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,11 +1,13 @@
 type ButtonProps = {
+  className?: string;
   borderColor: string;
   text: string;
+  onClick?: () => void; 
 };
 
-const Button: React.FC<ButtonProps> = ({ borderColor, text }) => {
+const Button: React.FC<ButtonProps> = ({ borderColor, text, onClick, className="" }) => {
   return (
-    <div className={`text-white italic inline-block text-center p-4 border-4 rounded-full w-sm:w-1/8 text-xl sm:text-2xl ${borderColor} bg-black-200`}>
+    <div onClick={onClick} className={`cursor-pointer text-white italic inline-block text-center p-4 border-4 rounded-full w-sm:w-1/8 text-xl sm:text-2xl ${borderColor} bg-black-200 ${className}`}>
       {text}
     </div>
   );
