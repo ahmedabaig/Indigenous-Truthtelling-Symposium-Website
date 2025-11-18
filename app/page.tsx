0908f142b2f34228ad8d51199e674d78129ae7c3
Johnny Wu <johwuy@gmail.com>
2025-11-18 00:51:31 +0000

    Use paragraphs component

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,15 @@ import Divider from "./components/divider";
 import RefaatAlareer from "./components/refaat-alareer";
 import WeshoyotAlvitre from "./components/weshoyot-alvitre";
 import MonaKadah from "./components/mona-kadah";
+import Paragraphs from "./components/paragraphs";
 
 export default function Home() {
+  const paragraphs = [
+    "Indigenous Truthtelling brings together past and ongoing legacies of resistance to colonialism to educate about Indigenous resistance movements - with distinct homelands, languages and cultures – including Indigenous residents of Turtle Island and the Palestinian shatat, diaspora, as well as their co-strugglers.",
+    "Recognizing the ongoing anti-colonial struggles of Indigenous people around the world, we envision this as one step toward a shared liberated horizon.",
+    "We hope that teachers, educators, students, laity, community members and people of conscience will share and educate others with these artistic pieces, poems and mosaics."
+  ];
+
   return (
     <main className="text-center flex flex-col min-h-screen bg-background items-center">
       <p className="m-15 mt-30 pt-12 md:m-30 mt:mt-30 md:mt-24 md:pt-24 text-3xl/relaxed text-creme">An educational art project, providing resources to students, activists, educators and community. Promoting resilience, resistance, and support for indigenous communities.</p>
@@ -31,22 +38,8 @@ export default function Home() {
       </div>
       <h2 className="text-green-300 text-5xl/relaxed italic mb-10">About the Indigenous Truthtelling Project</h2>
       <Divider color1="#7B9F49"></Divider>
-      <div className="my-25 flex flex-col gap-10 text-left text-creme text-2xl/relaxed mx-30">
-        <p>
-          Indigenous Truthtelling Project brings together past and ongoing legacies of resistance to colonialism and a commitment to indigenous knowledge keepers.
-        </p>
-        <p>
-          This project seeks to educate about indigenous resistance movements that may be separated by language and geography, but are united in their struggle in the face of colonial oppression, ethnic cleansing, genocide, scholasticide and other expressions of systemic racism.
-        </p>
-        <p>
-          We have created sites of engagement for the public to use, to commemorate, honor, memorialize and mourn our heroic martyrs: journalists, educators, writers and poets.
-        </p>
-        <p>
-          We honor their memory through various forms of creative expression, recognizing the ongoing struggles of indigenous people around the world, including indigenous residents of Turtle Island and the Palestinian shatat, diaspora, as well as their co-strugglers.
-        </p>
-        <p>
-          We hope that teachers, educators, students, laity, community members and people of conscience will share and educate others with these artistic pieces, poems and mosaics.
-        </p>
+      <div className="my-12 md:my-24 mx-10 md:mx-30">
+        <Paragraphs paragraphs={paragraphs}></Paragraphs>
       </div>
       <Divider color1="#7B9F49"></Divider>
       <MonaKadah />
